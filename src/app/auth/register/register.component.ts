39d1a6def9cd4c7cad6e0fdfc87f2e86ab4be678
dvrepa <d.v.repnikov@gmail.com>
2022-06-13 11:28:05 +0300

    Добавил NgRx

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from 'src/app/auth/store/actions/register.action';
 
 @Component({
   selector: 'ma-register',
@@ -9,14 +11,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
   onSubmit(): void {
-    console.log(this.form.value);
+    this.store.dispatch(registerAction(this.form.value));
   }
 
   private initializeForm(): void {
